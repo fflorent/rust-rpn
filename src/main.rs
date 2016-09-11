fd77dff05feabd37eb5eade70dd8c1cfef2e524f
Florent FAYOLLE <florent.fayolle69@gmail.com>
2016-09-11 20:24:13 +0200

    Better error handling

--- src/main.rs
+++ src/main.rs
@@ -6,38 +6,44 @@ fn main() {
 
   loop {
     print!("> ");
-    io::stdout().flush();
+    io::stdout().flush().expect("flushing failed");
     let mut input = String::new();
 
     io::stdin().read_line(&mut input)
       .expect("failed to read line");
 
-    if input == "quit" {
+    if input.trim() == "quit" {
       break;
     }
-    println!("{}", rpl(&input));
+    let result = rpl(&input);
+    if result.is_err() {
+      println!("Error: {}", result.unwrap_err());
+    } else {
+      println!("{}", result.unwrap());
+    }
   }
 }
 
-fn rpl(expr: &str) -> f32 {
+fn rpl(expr: &str) -> Result<f32, &str> {
   let mut stack:Vec<f32> = Vec::new();
   for token in expr.split_whitespace() {
     let wrapped_operand = token.parse::<f32>();
     let is_operator = wrapped_operand.is_err();
     if is_operator {
       if stack.len() < 2 {
-        panic!("Unsufficient operands before operator");
+        return Err("Unsufficient operands before operator");
       }
-      // xxxFlorent: Any way to destructure like this? Not sure of what I am doing below
+      // xxxFlorent: Any way to destructure like this? Not sure of what I am
+      // doing below
       // let [ operand1, operand2 ] = stack.drain((token.len() - 3)..).collect();
-      let operand2 = stack.pop().expect("expected f32 values only in stack");
-      let operand1 = stack.pop().expect("expected f32 values only in stack");
+      let operand2 = stack.pop().expect("expected f32 values in stack");
+      let operand1 = stack.pop().expect("expected f32 values in stack");
       let result = match token {
         "+" => operand1 + operand2,
         "-" => operand1 - operand2,
         "*" => operand1 * operand2,
         "/" => operand1 / operand2,
-        _ => panic!("Unsupported operator")
+        _ => return Err("Unsupported operator")
       };
       stack.push(result);
     } else {
@@ -46,65 +52,63 @@ fn rpl(expr: &str) -> f32 {
   }
 
   if stack.len() != 1 {
-    panic!("Remaining untreated operands. Probably missing operator.");
+    return Err("Remaining untreated operands. Probably missing operator.");
   }
-  return stack.pop().expect("expected a f32 value remaining in stack");
+  return Ok(stack.pop().expect("expected a f32 value remaining in stack"));
 }
 
 #[test]
 fn it_adds() {
   let result = rpl("1 2 +");
-  assert_eq!(result, 3.0);
+  assert_eq!(result.unwrap(), 3.0);
 }
 
 #[test]
 fn it_substracts() {
   let result = rpl("1 2 -");
-  assert_eq!(result, -1.0);
+  assert_eq!(result.unwrap(), -1.0);
 }
 
 #[test]
 fn it_multiplies() {
   let result = rpl("6 7 *");
-  assert_eq!(result, 42.0);
+  assert_eq!(result.unwrap(), 42.0);
 }
 
 #[test]
 fn it_divides() {
   let result = rpl("1 2 /");
-  assert_eq!(result, 0.5);
+  assert_eq!(result.unwrap(), 0.5);
 }
 
 #[test]
 fn it_calculates_complex_expressions() {
   // ((1+2) * 8 / (5-1) - 1) / 2
   let result = rpl("1 2 + 8 * 5 1 - / 1 - 2 /");
-  assert_eq!(result, 2.5);
+  assert_eq!(result.unwrap(), 2.5);
 }
 
 #[test]
 fn it_allows_multiple_shitespaces() {
   let result = rpl("1  2 +\t3 -");
-  assert_eq!(result, 0.0);
+  assert_eq!(result.unwrap(), 0.0);
 }
 
 #[test]
-#[should_panic]
 fn it_panics_for_unsupported_characters() {
-  rpl("1 2 t");
-  // xxxFlorent: How to test the panic message is right?
+  let result = rpl("1 2 t");
+  assert_eq!(result.unwrap_err(), "Unsupported operator");
 }
 
 #[test]
-#[should_panic]
 fn it_panics_for_unsufficient_operands() {
-  rpl("1 +");
-  // xxxFlorent: How to test the panic message is right?
+  let result = rpl("1 +");
+  assert_eq!(result.unwrap_err(), "Unsufficient operands before operator");
 }
 
 #[test]
-#[should_panic]
 fn it_panics_for_unsufficient_operators() {
-  rpl("1 2 3 +");
-  // xxxFlorent: How to test the panic message is right?
+  let result = rpl("1 2 3 +");
+  assert_eq!(result.unwrap_err(),
+    "Remaining untreated operands. Probably missing operator.");
 }
