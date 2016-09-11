084d3086284ec996cdbd24b9feb71bedb7e867ae
Florent FAYOLLE <florent.fayolle69@gmail.com>
2016-09-11 18:47:06 +0200

    Implement repl

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,25 @@
-fn main(expr: &str) -> f32 {
+use std::io::{self, Write};
+
+fn main() {
+  println!("Reverse Polish Notation.");
+  println!("Type quit to exit");
+
+  loop {
+    print!("> ");
+    io::stdout().flush();
+    let mut input = String::new();
+
+    io::stdin().read_line(&mut input)
+      .expect("failed to read line");
+
+    if input == "quit" {
+      break;
+    }
+    println!("{}", rpl(&input));
+  }
+}
+
+fn rpl(expr: &str) -> f32 {
   let mut stack:Vec<f32> = Vec::new();
   for token in expr.split_whitespace() {
     let wrapped_operand = token.parse::<f32>();
@@ -32,58 +53,58 @@ fn main(expr: &str) -> f32 {
 
 #[test]
 fn it_adds() {
-  let result = main("1 2 +");
+  let result = rpl("1 2 +");
   assert_eq!(result, 3.0);
 }
 
 #[test]
 fn it_substracts() {
-  let result = main("1 2 -");
+  let result = rpl("1 2 -");
   assert_eq!(result, -1.0);
 }
 
 #[test]
 fn it_multiplies() {
-  let result = main("6 7 *");
+  let result = rpl("6 7 *");
   assert_eq!(result, 42.0);
 }
 
 #[test]
 fn it_divides() {
-  let result = main("1 2 /");
+  let result = rpl("1 2 /");
   assert_eq!(result, 0.5);
 }
 
 #[test]
 fn it_calculates_complex_expressions() {
   // ((1+2) * 8 / (5-1) - 1) / 2
-  let result = main("1 2 + 8 * 5 1 - / 1 - 2 /");
+  let result = rpl("1 2 + 8 * 5 1 - / 1 - 2 /");
   assert_eq!(result, 2.5);
 }
 
 #[test]
 fn it_allows_multiple_shitespaces() {
-  let result = main("1  2 +\t3 -");
+  let result = rpl("1  2 +\t3 -");
   assert_eq!(result, 0.0);
 }
 
 #[test]
 #[should_panic]
 fn it_panics_for_unsupported_characters() {
-  main("1 2 t");
+  rpl("1 2 t");
   // xxxFlorent: How to test the panic message is right?
 }
 
 #[test]
 #[should_panic]
 fn it_panics_for_unsufficient_operands() {
-  main("1 +");
+  rpl("1 +");
   // xxxFlorent: How to test the panic message is right?
 }
 
 #[test]
 #[should_panic]
 fn it_panics_for_unsufficient_operators() {
-  main("1 2 3 +");
+  rpl("1 2 3 +");
   // xxxFlorent: How to test the panic message is right?
 }
