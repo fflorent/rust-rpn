81ce89162e025a51879c9cc74b5be6161944f8bf
Florent FAYOLLE <florent.fayolle69@gmail.com>
2016-09-11 17:43:35 +0200

    More tests for panic cases

--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,24 @@ fn main(expr: &str) -> i32 {
     let wrapped_operand = token.parse::<i32>();
     let is_operator = wrapped_operand.is_err();
     if is_operator {
+      if stack.len() < 2 {
+        panic!("Unsufficient operands before operator");
+      }
       // xxxFlorent: Any way to destructure like this? Not sure of what I am doing below
       // let [ operand1, operand2 ] = stack.drain((token.len() - 3)..).collect();
       let operand1 = stack.pop().expect("expected i32 values only in stack");
       let operand2 = stack.pop().expect("expected i32 values only in stack");
       result += match token {
         "+" => operand1 + operand2,
-        _ => panic!("unsupported operator")
+        _ => panic!("Unsupported operator")
       }
     } else {
       stack.push(wrapped_operand.unwrap());
     }
+  }
+
+  if stack.len() > 0 {
+    panic!("Remaining untreated operands. Probably missing operator.");
   }
   return result;
 }
@@ -30,5 +37,19 @@ fn it_adds() {
 #[should_panic]
 fn it_panics_for_unsupported_operators() {
   main("1 2 =");
+  // xxxFlorent: How to test the panic message is right?
+}
+
+#[test]
+#[should_panic]
+fn it_panics_for_unsufficient_operands() {
+  main("1 +");
+  // xxxFlorent: How to test the panic message is right?
+}
+
+#[test]
+#[should_panic]
+fn it_panics_for_unsufficient_operators() {
+  main("1 2 3 +");
   // xxxFlorent: How to test the panic message is right?
 }
